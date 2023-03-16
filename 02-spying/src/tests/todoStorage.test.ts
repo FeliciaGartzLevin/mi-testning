45fe92d8f0ef6c44fded0e3aa5761327fd2f3d3c
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2023-03-16 14:05:55 +0100

    second lesson and first commit

---
+++ 02-spying/src/tests/todoStorage.test.ts
@@ -0,0 +1,46 @@
+import { afterEach, describe, expect, it, vi } from 'vitest'
+import mockLocalStorage from '../mocks/mockedLocalStorage'
+import { Todo } from '../types/Todo'
+import { getTodos, saveTodos } from '../utils/todoStorage'
+
+global.localStorage = mockLocalStorage()
+
+const TODO: Todo = {
+	id: 1,
+	title: 'My first todo',
+	completed: false,
+}
+
+describe('get todos', () => {
+	it('returns empty list of todos', () => {
+		// i spy
+		const getItemSpy = vi.spyOn(global.localStorage, 'getItem')
+
+		const todos = getTodos()
+
+		expect(getItemSpy).toHaveBeenCalled()
+		expect(todos.length).toBe(0)
+	})
+})
+
+describe('save todos', () => {
+
+	afterEach(() => {
+		global.localStorage.clear()
+	})
+
+	it.todo('can save a todo', () => {
+		const result = saveTodos([TODO])
+
+		expect(result.success).toBe(true)
+	})
+
+	it.todo('can save a todo and then get it', () => {
+		const result = saveTodos([TODO])
+
+		expect(result.success).toBe(true)
+
+		const todos = getTodos()
+		expect(todos).toContainEqual(TODO)
+	})
+})
