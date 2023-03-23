7b16f78c7157ab3e78e0bc2ea9538fc46dcaabb5
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2023-03-23 13:29:15 +0100

    mocking parts of the API with MSW (Mock Service Worker)

---
+++ 03-integration-testing/frontend/src/mocks/handlers.ts
@@ -0,0 +1,54 @@
+import { rest } from 'msw'
+import { Todo, CreateTodoData } from '../types/Todo'
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
+
+const dummyTodos: Todo[] = [
+	{ id: 1, title: 'My first todo', completed: false },
+	{ id: 2, title: 'My second todo', completed: true },
+]
+
+export const handlers = [
+	// Mock get all todos
+	// GET http://localhost:3001/todos
+	rest.get(BASE_URL + '/todos', (_req, res, ctx) => { //_ för att få bort squiggly lines när den inte används men måste vara en parameter
+		return res(
+			ctx.status(200),
+			ctx.json(dummyTodos)
+		)
+	}),
+
+	// Mock get single todo
+	// GET http://localhost:3001/todos/:id
+
+	// Mock create todo
+	// POST http://localhost:3001/todos
+	rest.post(BASE_URL + '/todos', async (req, res, ctx) => {
+		const payload = await req.json<CreateTodoData>()
+
+		// find next id
+		const id = Math.max(...dummyTodos.map(todo => todo.id)) + 1
+
+		// create our new dummy todo
+		const todo: Todo = {
+			id: id,
+			title: payload.title,
+			completed: payload.completed,
+		}
+
+		// add dummy todo to list of dummy todos
+		dummyTodos.push(todo)
+
+		// respond with dummy todo
+		return res(
+			ctx.status(201),
+			ctx.json(todo)
+		)
+	})
+
+	// Mock update todo
+	// PATCH http://localhost:3001/todos/:id
+
+	// Mock delete todo
+	// DELETE http://localhost:3001/todos/:id
+]
--- 03-integration-testing/frontend/src/tests/TodoAPI.test.ts
+++ 03-integration-testing/frontend/src/tests/TodoAPI.test.ts
@@ -1,6 +1,23 @@
-import { describe, it, expect, } from 'vitest'
+import { afterAll, afterEach, beforeAll, describe, it, expect, } from 'vitest'
+import { server } from '../mocks/server'
 import * as TodoAPI from '../services/TodoAPI'
 import { CreateTodoData, UpdateTodoData } from '../types/Todo'
+
+// 🏎️ Boot API mocking
+beforeAll(() => {
+	server.listen()
+})
+
+// 🧨 Reset handlers
+afterEach(() => {
+	server.resetHandlers()
+})
+
+// 🧹 Clean up after ourselves
+afterAll(() => {
+	server.close()
+})
+
 
 const newTodo: CreateTodoData = {
 	title: 'Test todo',
@@ -14,6 +31,7 @@ describe('TodoAPI', () => {
 
 		expect(Array.isArray(todos)).toBe(true)
 	})
+
 
 	it('should create a todo', async () => {
 
@@ -32,56 +50,57 @@ describe('TodoAPI', () => {
 		// expect(todo.completed).toBe(newTodo.completed)
 
 	})
-
-	it('should create and then get the todo', async () => {
-
-		const todo = await TodoAPI.createTodo(newTodo)
-		const fetchedTodo = await TodoAPI.getTodo(todo.id)
-
-		expect(fetchedTodo).toEqual(todo)
-
-	})
-
-	it('should create and then find the todo among all todos', async () => {
-
-		const todo = await TodoAPI.createTodo(newTodo)
-		const fetchedTodos = await TodoAPI.getTodos()
-
-		// min lösning:
-		// const foundTodo = fetchedTodos.find(foundtodo => todo.id === foundtodo.id)
-		// expect(foundTodo).toEqual(todo)
-
-		// johans lösning:
-		expect(fetchedTodos).toContainEqual(todo)
-
-	})
-
-	it('should create and then update the todo', async () => {
-
-		const todo = await TodoAPI.createTodo(newTodo)
-
-		const updatedTodo = await TodoAPI.updateTodo(todo.id, {
-			title: 'Stop testing',
-			completed: true
+	/*
+		it('should create and then get the todo', async () => {
+	
+			const todo = await TodoAPI.createTodo(newTodo)
+			const fetchedTodo = await TodoAPI.getTodo(todo.id)
+	
+			expect(fetchedTodo).toEqual(todo)
+	
 		})
-
-		expect(updatedTodo).toMatchObject({
-			id: todo.id,
-			title: 'Stop testing',
-			completed: true,
+	
+	
+		it('should create and then find the todo among all todos', async () => {
+	
+			const todo = await TodoAPI.createTodo(newTodo)
+			const fetchedTodos = await TodoAPI.getTodos()
+	
+			// min lösning:
+			// const foundTodo = fetchedTodos.find(foundtodo => todo.id === foundtodo.id)
+			// expect(foundTodo).toEqual(todo)
+	
+			// johans lösning:
+			expect(fetchedTodos).toContainEqual(todo)
+	
 		})
-
-	})
-
-	it('should create and then delete the todo', async () => {
-		const todo = await TodoAPI.createTodo(newTodo)
-
-		await TodoAPI.deleteTodo(todo.id)
-
-		const todos = await TodoAPI.getTodos()
-
-		expect(todos).not.toContainEqual(todo)
-
-	})
+	
+		it('should create and then update the todo', async () => {
+	
+			const todo = await TodoAPI.createTodo(newTodo)
+	
+			const updatedTodo = await TodoAPI.updateTodo(todo.id, {
+				title: 'Stop testing',
+				completed: true
+			})
+	
+			expect(updatedTodo).toMatchObject({
+				id: todo.id,
+				title: 'Stop testing',
+				completed: true,
+			})
+	
+		})
+	
+		it('should create and then delete the todo', async () => {
+			const todo = await TodoAPI.createTodo(newTodo)
+	
+			await TodoAPI.deleteTodo(todo.id)
+	
+			const todos = await TodoAPI.getTodos()
+	
+			expect(todos).not.toContainEqual(todo)
+	
+		})*/
 
 })
