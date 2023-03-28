37e09840722c5c3903ea56afad973bcc1dea8cef
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2023-03-28 13:50:55 +0200

    finished WS of e2e tests w cypress

--- 05-e2e-testing/frontend/cypress/e2e/todos.cy.ts
+++ 05-e2e-testing/frontend/cypress/e2e/todos.cy.ts
@@ -1,5 +1,46 @@
-describe('Todo', () => {
-	it('passes', () => {
+const insertTodo = 'Make e2e test'
+
+describe('Todos', () => {
+	beforeEach(() => {
 		cy.visit('/')
 	})
+
+	it('should see at least one todo', () => {
+		cy.get('#todos')
+			.find('li')
+			.should('have.length.at.least', 1)
+	})
+
+	context('create todo', () => {
+
+		it('create todo form should be empty', () => {
+			cy.get('#new-todo-title')
+				.should('be.empty')
+
+		})
+
+		it('cant create a todo without a title', () => {
+
+			cy.get('#new-todo-title').should('be.empty')
+			cy.get('.btn-success').click()
+			cy.get('#error').should('exist').contains('Title cannot be empty')
+		})
+
+		it('can create a new todo (and see it in the list and clears input)', () => {
+			cy.get('#new-todo-title').type(insertTodo)
+			cy.get('.btn-success').click()
+			cy.get('#todos').contains(insertTodo)
+
+		})
+
+		it('can type in the create todo form and then reset the form', () => {
+			cy.get('#new-todo-title').type(insertTodo)
+			cy.get('.btn-warning').click()
+			cy.get('#new-todo-title')
+				.should('be.empty')
+
+		})
+	})
+
+
 })
